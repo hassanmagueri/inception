d526da99cc9fb8600cc470572cc0974bfa74f4d8
hassanmagueri <hassanmagueri2001@gmail.com>
2025-01-04 06:41:09 -0500

    set env file in mariadb container

--- srcs/requirements/mariadb/container_run.sh
+++ srcs/requirements/mariadb/container_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-# docker rm -f  $(docker ps -qa);
+docker rm -f  $(docker ps -qa);
 docker build -t maria .;
 docker run -d --env-file ../../.env --name maria -p 3306:3306 maria;
+# docker run -it --env-file ../../.env --name maria -p 3306:3306 maria bash;
