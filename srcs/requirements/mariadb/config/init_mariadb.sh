7362f2338cce3a65ed927ed06f1d74d36c3881c3
hassanmagueri <hassanmagueri2001@gmail.com>
2025-01-05 15:23:07 -0500

    resolve probelem the container mariadb stop after 30 s

--- srcs/requirements/mariadb/config/init_mariadb.sh
+++ srcs/requirements/mariadb/config/init_mariadb.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+# service mariadb start;
+
+mariadbd &
+sleep 5
+PID=$!
 
 mysql << EOF
-    CREATE USER '$USER_NAME'@'%' IDENTIFIED BY 'aa'; 
+   CREATE OR REPLACE USER '$USER_NAME'@'%' IDENTIFIED BY 'aa'; 
     GRANT ALL PRIVILEGES ON *.* TO 'aa'@'%' WITH GRANT OPTION; 
     FLUSH PRIVILEGES;
 EOF
 
-mariadbd;
+# killall mariadbd
+wait $PID 
