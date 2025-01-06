18e43a7d9d48c00ecade9f7e410c7a03c1ed945e
hassanmagueri <hassanmagueri2001@gmail.com>
2025-01-06 06:44:19 -0500

    resolve probelem the container mariadb stop after 30 s

--- srcs/requirements/mariadb/config/init_mariadb.sh
+++ srcs/requirements/mariadb/config/init_mariadb.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
-# service mariadb start;
 
-mariadbd &
-sleep 5
-PID=$!
+# mariadbd &
+# sleep 5
+# PID=$!
 
-mysql << EOF
-   CREATE OR REPLACE USER '$USER_NAME'@'%' IDENTIFIED BY 'aa'; 
-    GRANT ALL PRIVILEGES ON *.* TO 'aa'@'%' WITH GRANT OPTION; 
-    FLUSH PRIVILEGES;
-EOF
+# mysql << EOF
+#    CREATE OR REPLACE USER '$USER_NAME'@'%' IDENTIFIED BY 'aa'; 
+#     GRANT ALL PRIVILEGES ON *.* TO 'aa'@'%' WITH GRANT OPTION; 
+#     FLUSH PRIVILEGES;
+# EOF
 
-# killall mariadbd
-wait $PID 
+# wait $PID 
+
+
+# mysql << EOF
+#    CREATE OR REPLACE USER '$USER_NAME'@'%' IDENTIFIED BY 'aa'; 
+#     GRANT ALL PRIVILEGES ON *.* TO 'aa'@'%' WITH GRANT OPTION; 
+#     FLUSH PRIVILEGES;
+# EOF
+
+service mariadb start; # start the service in background
+sleep 2;
+mysql -u root  -e "CREATE OR REPLACE USER '$USER_NAME'@'%' IDENTIFIED BY 'aa'"
+mysql -u root  -e "GRANT ALL PRIVILEGES ON *.* TO '$USER_NAME'@'%' WITH GRANT OPTION"
+mysql -u root  -e "FLUSH PRIVILEGES"
+
+killall -9 mariadbd; # stop the service
+
+mariadbd # start the service in foreground
