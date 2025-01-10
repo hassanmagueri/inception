dedc4464b615b15b20a766e9b9c422941ca5c1dd
hassanmagueri <hassanmagueri2001@gmail.com>
2025-01-10 13:40:30 -0500

    wordpress container

--- srcs/requirements/mariadb/config/init_mariadb.sh
+++ srcs/requirements/mariadb/config/init_mariadb.sh
@@ -21,8 +21,8 @@
 
 service mariadb start; # start the service in background
 sleep 2;
-mysql -u root  -e "CREATE OR REPLACE USER '$USER_NAME'@'%' IDENTIFIED BY 'aa'"
-mysql -u root  -e "GRANT ALL PRIVILEGES ON *.* TO '$USER_NAME'@'%' WITH GRANT OPTION"
+mysql -u root  -e "CREATE OR REPLACE USER '$NAME_USER'@'%' IDENTIFIED BY '$PASSWORD_ROOT'"
+mysql -u root  -e "GRANT ALL PRIVILEGES ON *.* TO '$NAME_USER'@'%' WITH GRANT OPTION"
 mysql -u root  -e "FLUSH PRIVILEGES"
 
 killall -9 mariadbd; # stop the service
