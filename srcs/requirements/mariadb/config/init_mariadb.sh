469b3648a97d0e4b1959cbfbb894770bd0a514de
hassanmagueri <hassanmagueri2001@gmail.com>
2025-01-11 11:06:38 -0500

    the server seems work

--- srcs/requirements/mariadb/config/init_mariadb.sh
+++ srcs/requirements/mariadb/config/init_mariadb.sh
@@ -21,8 +21,9 @@
 
 service mariadb start; # start the service in background
 sleep 2;
-mysql -u root  -e "CREATE OR REPLACE USER '$NAME_USER'@'%' IDENTIFIED BY '$PASSWORD_ROOT'"
-mysql -u root  -e "GRANT ALL PRIVILEGES ON *.* TO '$NAME_USER'@'%' WITH GRANT OPTION"
+mysql -u root -e "CREATE DATABASE $NAME_DATABASE"
+mysql -u root  -e "CREATE OR REPLACE USER '$NAME_USER'@'%' IDENTIFIED BY '$USER_PASSWORD'"
+mysql -u root  -e "GRANT ALL PRIVILEGES ON $NAME_DATABASE.* TO '$NAME_USER'@'%' WITH GRANT OPTION"
 mysql -u root  -e "FLUSH PRIVILEGES"
 
 killall -9 mariadbd; # stop the service
