fb34e47d9e9eed29f41fd388fbd207c048c3b143
hassanmagueri <hassanmagueri2001@gmail.com>
2025-01-23 07:53:15 -0500

    config redis

--- srcs/requirements/bonus/ftp/script.sh
+++ srcs/requirements/bonus/ftp/script.sh
@@ -2,4 +2,6 @@
 
 docker build -t ftp .  
 
-docker run --privileged  -it -p 20:20 -p 21:21 -p 990:990 -p 40000:40000 -p 50000:50000 ftp bash
+# docker run --privileged  -it -p 20:20 -p 21:21 -p 990:990 -p 40000:40000 -p 50000:50000 ftp bash
+
+docker run -d -p 20:20 -p 21:21 -p 990:990 -p 40000-40100:40000-40100 --env-file /home/emagueri/inception/srcs/.env ftp
---
+++ srcs/requirements/bonus/grafana/config/init_grafana.sh
@@ -0,0 +1,60 @@
+#!bin/bash
+# apt-get install -y apt-transport-https software-properties-common wget
+# mkdir -p /etc/apt/keyrings/
+# wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
+# echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee -a /etc/apt/sources.list.d/grafana.list
+# echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com beta main" | tee -a /etc/apt/sources.list.d/grafana.list
+
+# # Installs the latest OSS release:
+# apt-get update -y
+# apt-get install grafana -y
+
+cat << EOF > /etc/grafana/provisioning/dashboards/mydashboards.yaml
+apiVersion: 1
+providers:
+- name: 'default'
+  orgId: 1
+  folder: ''
+  type: file
+  disableDeletion: false
+  editable: true
+  options:
+    path: /var/lib/grafana/dashboards
+EOF
+
+
+cat << EOF >  /etc/grafana/provisioning/datasources/prometheus.yaml
+apiVersion: 1
+datasources:
+- name: Prometheus
+  type: prometheus
+  url: http://localhost:9090
+  access: proxy
+  isDefault: true
+EOF
+
+sleep 1
+
+cat << EOF >  /etc/grafana/grafana.ini
+[auth.anonymous]
+enabled = true
+org_name = Main Org.
+org_role = Viewer
+
+[auth]
+disable_login_form = true
+disable_signout_menu = true
+
+[dashboards]
+default_home_dashboard_path = /var/lib/grafana/dashboards/dashboard.json
+EOF
+
+
+prometheus --config.file="prometheus.yml" &
+prometheus-node-exporter &
+# grafana-server --homepath=/usr/share/grafana --config=/etc/grafana/grafana.ini & 
+/usr/share/grafana/bin/grafana server --homepath=/usr/share/grafana --config=/etc/grafana/grafana.ini --packaging=deb \
+    cfg:default.paths.provisioning=/etc/grafana/provisioning cfg:default.paths.data=/var/lib/grafana \
+    cfg:default.paths.logs=/var/log/grafana cfg:default.paths.plugins=/var/lib/grafana/plugins
+
+tail -f /dev/null # ??
