0bbb3ed21bbe6f4f0851e44449584c86193894ae
hassanmagueri <hassanmagueri2001@gmail.com>
2025-01-31 13:39:58 -0500

    add cadvisor

--- srcs/requirements/bonus/ftp/config/init_ftp.sh
+++ srcs/requirements/bonus/ftp/config/init_ftp.sh
@@ -1,31 +1,27 @@
-#!/bin/bash
-
-# ufw allow 20/tcp
-# ufw allow 21/tcp
-# ufw allow 990/tcp
-# ufw allow 40000:50000/tcp
-
+#! /bin/bash
 
 useradd -m -d $FTP_HOME_DIR -s /bin/bash $FTP_USER;
 echo "$FTP_USER:$(cat /run/secrets/ftp_pass)" | chpasswd;
 
 mkdir -p /var/run/vsftpd/empty #?
+chmod 755 /var/run/vsftpd/empty
 
 
-chmod 777 $FTP_HOME_DIR
 
 # echo "IP : " $(hostname -I)
 mkdir $FTP_HOME_DIR/ftp
 
+chmod 777 -R $FTP_HOME_DIR 
+
 echo "test" > $FTP_HOME_DIR/ftp/file
 
 
+echo "listen=YES
+local_enable=YES
+write_enable=YES
+pasv_enable=YES
+pasv_min_port=40000
+pasv_max_port=40100
+pasv_address=$URL" > /etc/vsftpd.conf
 
-vsftpd \
-    -olisten=YES \
-    -owrite_enable=YES \
-    -opasv_enable=YES \
-    -opasv_min_port=40000 \
-    -opasv_max_port=40100 \
-    -opasv_address="$URL" \
-    -oftpd_banner="Welcome to the FTP Server!"
+vsftpd /etc/vsftpd.conf
