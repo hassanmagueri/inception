853f53baea081e81b7b0a6a41b1994bbb500d9db
hassanmagueri <hassanmagueri2001@gmail.com>
2025-02-01 11:53:20 -0500

    add cadvisor

--- srcs/requirements/mariadb/config/init_mariadb.sh
+++ srcs/requirements/mariadb/config/init_mariadb.sh
@@ -7,7 +7,7 @@ sleep 1;
 mysql -u root -e "CREATE DATABASE $DB_NAME"
 # mysql -u root  -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'"
 mysql -u root  -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$(cat /run/secrets/db_pass)'"
-mysql -u root  -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' WITH GRANT OPTION"
+mysql -u root  -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%'"
 mysql -u root  -e "FLUSH PRIVILEGES"
 service mariadb stop
 mysqld_safe --port=3306 --bind-address=0.0.0.0
--- srcs/requirements/wordpress/conf/init_wp.sh
+++ srcs/requirements/wordpress/conf/init_wp.sh
@@ -6,7 +6,7 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/bin/wp
 
-sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf #?
 
 
 
